4cdd2800d9b549549f25fc4c5af4b643dbddc517
Santiago Gelvez <santiagogelvez29@gmail.com>
2024-09-17 12:56:43 -0500

    Change history on router

--- src/router/index.ts
+++ src/router/index.ts
@@ -4,7 +4,7 @@ import { useAlertLoading } from '@/composables/useAlert'
 
 
 const router = createRouter({
-    history: createWebHistory(import.meta.env.BASE_URL),
+    history: createWebHistory(),
     routes: [
         {
             path: '/',
